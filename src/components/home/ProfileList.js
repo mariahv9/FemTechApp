f9184c747f5ce2e1ddddbcebe27a5e970eb8fe3d
Maria Fernanda Hernandez Vargas <mf.hernandez@globant.com>
2023-04-23 11:27:27 -0500

    Adicion filter

--- src/components/home/ProfileList.js
+++ src/components/home/ProfileList.js
@@ -1,32 +1,61 @@
 import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import { Button } from '@mui/material';
 import Profiles from '../../helper/profiles.json';
+import Professions from '../../helper/professions.json';
+import { Button, TextField, MenuItem, ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
 
 export default function ProfileList() {
+  const [perfiles, setPerfiles] = React.useState(Profiles)
+
+  const [profesion, setProfesion] = React.useState('');
+
+  const handleChangeProfesion = async (event) => {
+    perfiles[event.target.name] = event.target.value;
+    //console.log(perfiles[event.target.name])
+    console.log(perfiles.map(perfil => perfil.profession = perfiles[event.target.name]))
+    setPerfiles(perfiles);
+    setProfesion(event.target.value);
+  };
+
   return (
-    <ImageList cols={4} gap={15}>
-      {Profiles.map((item) => (
-        <ImageListItem key={item.img} sx={{ width: 300, height: 300, borderRadius: '16px' }}>
-          <ImageListItemBar
-            title={item.first_name}
-            subtitle={item.last_name}
-            actionIcon={
-              <Button sx={{ color: 'white' }}>
-                Ver Perfil
-              </Button>
-            }
-          />
-          <img
-            src={`${item.profile_pic}?w=164&h=164&fit=crop&auto=format`}
-            srcSet={`${item.profile_pic}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.first_name}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    <div>
+      <div>
+        <TextField
+          id="filled-select-profession"
+          height="250"
+          select
+          label="Selecciona la profesion"
+          variant="filled"
+          value={profesion}
+          onChange={handleChangeProfesion}
+        >
+          {Professions.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      </div>
+      <ImageList cols={4} gap={15}>
+        {perfiles.map((item) => (
+          <ImageListItem key={item.img} sx={{ width: 300, height: 300, borderRadius: '16px' }}>
+            <ImageListItemBar
+              title={item.first_name + " " + item.last_name}
+              subtitle={item.profession}
+              actionIcon={
+                <Button sx={{ color: 'white' }}>
+                  Ver Perfil
+                </Button>
+              }
+            />
+            <img
+              src={`${item.profile_pic}?w=164&h=164&fit=crop&auto=format`}
+              srcSet={`${item.profile_pic}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.first_name}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </div>
   );
 }
--- src/components/home/ProfileSelector.js
+++ src/components/home/ProfileSelector.js
@@ -1,11 +1,35 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
 import Professions from '../../helper/professions.json';
-
+import MenuItem from '@mui/material/MenuItem';
+import Profiles from '../../helper/profiles.json';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import { Button } from '@mui/material';
 
 export default function ProfileSelector() {
+  const [perfiles, setPerfiles] = React.useState(
+    {
+      "id": 0,
+      "first_name": '',
+      "last_name": '',
+      "email": '',
+      "mentor": false,
+      "profession": '',
+      "profile_pic": ''
+    }
+  )
+
+  const [profesion, setprofesion] = React.useState('');
+
+  const handleChangeProfesion = async (event) => {
+    perfiles[event.target.name] = event.target.value;
+    setPerfiles(perfiles);
+    setprofesion(event.target.value);
+  };
+
   return (
     <Box
       component="form"
